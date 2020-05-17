ab6def20d25d1bf04c3b9c598a9b065b9a1bd297
EdManDev <ed.manigat@gmail.com>
2020-05-17 13:07:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/EdManDev/PyMongo_HelloWorld.git
+
+pip install pymongo
---
+++ connect.py
@@ -0,0 +1,14 @@
+# import pymongo
+# from pymongo import MongoClient
+
+# cluster = MongoClient(
+#     "mongodb+srv://edpy:edpy@pymongo-yks8h.mongodb.net/test?retryWrites=true&w=majority")
+
+
+# db = cluster["test"]
+# collection = db["test"]
+
+# post = {"_id": 0, "name": "Jhon", "lastname": "Doe", "Score": 70}
+
+# collection.insert_one(post)
+print("hello, world")
